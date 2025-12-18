95455ebed9b7ce93419e0059186338b33efdc658
luisvillada <otrodeluisvillada@gmail.com>
2025-12-18 15:56:18 -0500

    se implementa el primer listener para agregar contactos

--- src/interaz/VentanaPrincipal.java
+++ src/interaz/VentanaPrincipal.java
@@ -2,51 +2,67 @@ package interaz;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class VentanaPrincipal {
     //le asigno a una variable el objeto JFrame para que pueda acceder a sus metodos y construir la interfaz
-    private JFrame window;
+    private JFrame ventana;
 
     //Constructor
     public VentanaPrincipal(){
         //Primero
-        window = new JFrame();
-        window.setTitle("Agenda");
+        ventana = new JFrame();
+        ventana.setTitle("Agenda");
 
         //Esto va a Oculta la ventana y libera recursos nativos, no la cierra
-        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        ventana.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         //aca le asigno las dimensiones a la ventana
-        window.setSize(800, 500);
+        ventana.setSize(800, 500);
         //Aqui centro la app a la pantalla
-        window.setLocationRelativeTo(null);
+        ventana.setLocationRelativeTo(null);
 
-        //container
+        //container de los botones
         JPanel containerBotones = new JPanel();
         //FlowLayout.CENTER /.LEFT .RIGHT ETC - Define la alineación de los componentes dentro del panel
         //10 - Es el espacio horizontal en píxeles entre los componentes.
         // 5 - Es el espacio vertical (en píxeles) entre las filas de componentes.
         containerBotones.setLayout(new FlowLayout( FlowLayout.CENTER, 10, 5));
+        containerBotones.setBackground(Color.LIGHT_GRAY);
+
+
+        //container de contenido
         JPanel containerLista = new JPanel();
         containerLista.setLayout(new FlowLayout(FlowLayout.LEFT, 10,5));
-        //containerLista.setSize(750,450);
         containerLista.setBackground(Color.GRAY);
 
-        containerBotones.setBackground(Color.LIGHT_GRAY);
+        //Botones
         Button botonVerContactos = new Button("Ver Contactos");
         Button botonAgregarContacto = new Button("Agregar Contacto");
+
+        botonAgregarContacto.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JPanel containerAgregar = new JPanel();
+                containerAgregar.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
+                containerAgregar.setBackground(Color.white);
+                System.out.println("Se ha presionado el boton para agregar un nuevo contacto");
+            }
+        });
+
+        // Se agregan los botones a su container
         containerBotones.add(botonAgregarContacto);
-
-
-
         containerBotones.add(botonVerContactos);
-        //Aca ajusto la posicion del container de botones
-        window.add(containerLista, BorderLayout.CENTER);
-        window.add(containerBotones, BorderLayout.NORTH);
+
+        //Aca agrego los containers a la ventana  ajusto la posicion del container de botones
+
+        ventana.add(containerLista, BorderLayout.CENTER);
+        ventana.add(containerBotones, BorderLayout.NORTH);
 
 
 
         //mostrar la agenda
-        window.setVisible(true);
+        ventana.setVisible(true);
     }
 
 
